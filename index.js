ad0a1587a6c154f67a9d6fbecfad88626e7be8da
Jonathan Prada <jonathanprada@protonmail.ch>
2019-04-04 22:16:32 +0200

    added urbandictionary api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require("config");
 const Discord = require("discord.js");
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -37,19 +38,87 @@ client.on("message", message => {
     message.channel.send("Meow Meow");
   }
 
-  if (message.content === "gives milk") {
-    message.channel.send("*drinks*");
-  }
+  if (message.content.slice(0, 9) === "urbandict") {
+    const sentence = message.content.slice(10).toLowerCase();
 
-  if (message.content === "urbandict") {
-    console.log("test");
-    const keyword = message.content.slice(10);
     axios
-      .get(`http://api.urbandictionary.com/v0/define?term=${keyword}`)
+      .get(`http://api.urbandictionary.com/v0/define?term=${sentence}`)
       .then(response => {
-        message.channel.send(response.list[0].definition);
+        if (sentence.includes("shreyas")) {
+          message.channel.send(
+            "```" +
+              "A sexual position involving a laptop, where you code and then compile when you cum, i just did a [shreya]" +
+              "```"
+          );
+        } else if (sentence.includes("kishan")) {
+          message.channel.send(
+            "```" + "Legendary guy with an above average [asshole] size" + "```"
+          );
+        } else if (sentence.includes("munsterberg")) {
+          message.channel.send(
+            "```" +
+              "A guy who kicks homeless men in the nutsack when they are not watching, i just [munsterberged] that dude!" +
+              "```"
+          );
+        } else if (sentence.includes("mivo")) {
+          message.channel.send(
+            "```" +
+              "The vehicle arnold schwarzenegger requests on back-up" +
+              "```"
+          );
+        } else if (sentence.includes("chris")) {
+          message.channel.send(
+            "```" +
+              "Youtuber known for making noobs grab their ball sacks and pillage the software industry with their new skills" +
+              "```"
+          );
+        } else if (sentence.includes("takashi")) {
+          message.channel.send(
+            "```" +
+              "A type of nebraskan wild salmon recipe. I just had a [takashi] at a sushi place downtown" +
+              "```"
+          );
+        } else if (sentence.includes("keith")) {
+          message.channel.send(
+            "```" +
+              "The nicest guy around. [Keith] that type of guy to fail university so you could copy his code" +
+              "```"
+          );
+        } else if (sentence.includes("jax")) {
+          message.channel.send(
+            "```" +
+              "All the frustration, anger and plight of a person during their journey bottled up. After years, i've learnt to bottle my [jax]" +
+              "```"
+          );
+        } else if (sentence.includes("alex")) {
+          message.channel.send(
+            "```" +
+              "Young fella with a lot of potential in the industry. Yo dude, i think i saw [alex] auditioning for pornhub" +
+              "```"
+          );
+        } else if (sentence.includes("cory")) {
+          message.channel.send(
+            "```" +
+              "Dude with a massive c*ck, often quite clumsy. Dude [cory] just slapped my butt with his d*ck" +
+              "```"
+          );
+        } else if (sentence.includes("andy")) {
+          message.channel.send(
+            "```" +
+              "A god fearing keyboard dwelling rodent into hacking" +
+              "```"
+          );
+        } else {
+          message.channel.send(
+            "```" + response.data.list[0].definition + "```"
+          );
+        }
+
+        // message.channel.send(
+        //   "```" + "when shreyas gets trippy and lick arse with kisan" + "```"
+        // );
       })
-      .catch(() => message.channel.send("meow??"));
+      .catch(err => message.channel.send("meow??"));
   }
 });
 
